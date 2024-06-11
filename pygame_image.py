99985803009cc4f9dd0bb630487be08015fb1f89
C0A23092 <c0a23092cf@edu.teu.ac.jp>
2024-06-11 17:31:41 +0900

    move_ip １回使用

--- pygame_image.py
+++ pygame_image.py
@@ -16,6 +16,8 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     tmr = 0
+    kk_x = 0
+    kk_y = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -28,14 +30,16 @@ def main():
     
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            kk_y = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            kk_y = 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            kk_x = -1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2, 0)
-        kk_rct.move_ip(-1, 0)
+            kk_x = 2
+        kk_rct.move_ip(kk_x-1, kk_y)
+        kk_x = 0
+        kk_y = 0
         screen.blit(kk_img, kk_rct)  #kk_imgをkk_rctの設定で貼り付け
 
         pg.display.update()
